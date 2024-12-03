e86827d885836af8284dc93776c09b484c2b7ac8
hakim-77 <nur23ti@mahasiswa.pcr.ac.id>
2024-12-03 20:48:00 +0700

    Membuat Tampilan Halaman Awal

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+    public function home()
+    {
+        return view('KantinAfwah.home');
+    }
+    public function ourbrands()
+    {
+        return view('KantinAfwah.ourbrands');
+    }
+    public function about_us()
+    {
+        return view('KantinAfwah.about');
+    }
+    public function news()
+    {
+        return view('KantinAfwah.news&event');
+    }
+    public function career()
+    {
+        return view('KantinAfwah.career');
+    }
+    public function contact_us()
+    {
+        return view('KantinAfwah.contact');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,18 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
+Route::get('/', [HomeController::class, 'home'])->name('KantinAfwah.home');
+Route::get('/career', [HomeController::class, 'career'])->name('KantinAfwah.career');
+Route::get('/about_us', [HomeController::class, 'about_us'])->name('KantinAfwah.about_us');
+Route::get('/contact_us', [HomeController::class, 'contact_us'])->name('KantinAfwah.contact_us');
+Route::get('/news', [HomeController::class, 'news'])->name('KantinAfwah.news');
+Route::get('/ourbrands', [HomeController::class, 'ourbrands'])->name('KantinAfwah.ourbrands');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
