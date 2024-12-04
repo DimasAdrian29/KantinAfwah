edee647ddcd8f72e13013683f96ce12ae1428460
hakim-77 <nur23ti@mahasiswa.pcr.ac.id>
2024-12-04 21:23:38 +0700

    Memodifikasi Tampilan Website

--- resources/views/KantinAfwah/home.blade.php
+++ resources/views/KantinAfwah/home.blade.php
@@ -85,11 +85,11 @@
         <div class="row text-center">
             <div class="col-lg-6">
                 <h2>About Us</h2>
-                <p class="lead">We are a passionate group dedicated to providing exceptional food and service to our customers. Our mission is to create an unforgettable dining experience.</p>
+                <p class="lead">Kantin Afwah adalah tempat makan yang didirikan dengan tujuan untuk menyediakan hidangan lezat, bergizi, dan terjangkau untuk semua kalangan. Kami percaya bahwa makanan adalah salah satu elemen penting dalam menjalani kehidupan yang sehat dan bahagia. Oleh karena itu, setiap hidangan yang kami sajikan dipersiapkan dengan bahan-bahan berkualitas tinggi dan disajikan dengan cita rasa yang khas.</p>
                 <ul class="list-unstyled">
-                    <li><i class="fas fa-check-circle"></i> High-quality ingredients</li>
-                    <li><i class="fas fa-check-circle"></i> Family-friendly atmosphere</li>
-                    <li><i class="fas fa-check-circle"></i> Exceptional service</li>
+                    <li><i class="fas fa-check-circle"></i>Kualitas Terjamin</li>
+                    <li><i class="fas fa-check-circle"></i>Kebersihan Terjamin</li>
+                    <li><i class="fas fa-check-circle"></i>Pelayanan Yang Ramah</li>
                 </ul>
             </div>
             <div class="col-lg-6">
