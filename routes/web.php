90436c15cd51ff0a5aa4c235e3abd3a82ad5f008
DimasAdrian29 <dimas23ti@mahasiswa.pcr.ac.id>
2025-01-13 08:28:56 +0700

    CRUD PROMOSI

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\GaleryController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PromoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,5 +20,7 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::resource('galery', GaleryController::class)->only(['create','index', 'store', 'edit', 'update', 'destroy']);
+
+Route::resource('promo', PromoController::class)->only(['create','index', 'store', 'edit', 'update', 'destroy']);
 
 require __DIR__.'/auth.php';
